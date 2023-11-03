262679b2d75e7f298db6c82c74cc7b34d1222d55
Linus Meyer <e227436@sbl.ch>
2023-11-03 08:24:00 +0100

    Aufgabe 3, zählt alle kleinen "e"

--- script.js
+++ script.js
@@ -35,19 +35,19 @@ export function aufgabe02 (args) {
 export function aufgabe03 (args) {
   const input = args
   const result = []
+
+  let countE = 0
+
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
     if (currentElement === "e"){
       //zähle dieses e
-
+      countE = countE + 1
      }
     else if (currentElement === "E") {
       //zähle dieses E
-
+      
     }
   }
-  
-
- 
-  return result.join("")
+  return countE
 }
